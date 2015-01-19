15412150cf2ef33e5f84ed4c1d316a9b7ae8095f
Olli Vatsia <olli.vatsia@helsinki.fi>
2015-01-20 01:32:51 +0200

    made average calculation for ratings and some updates to view

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,14 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    amount = 0
+
+    ratings.each do |rate|
+      amount += rate.score
+    end
+
+    amount / ratings.count
+  end
 end
