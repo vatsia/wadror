aa1c25bc2c695fc305db947bd9a361fdf6f6013e
Olli Vatsia <olli.vatsia@helsinki.fi>
2015-01-20 00:56:11 +0200

    new methods to brewery

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,16 @@
 class Brewery < ActiveRecord::Base
   has_many :beers
+
+  def print_report
+    puts name
+    puts "established at year #{year}"
+    puts "number of beer #{beers.count}"
+  end
+
+  def restart
+    self.year = 2015
+    puts "changed to year #{year}"
+  end
+
+
 end
