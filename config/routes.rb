00ce4359d891615f903e4c078bd9a3a4be369519
Olli Vatsia <olli.vatsia@helsinki.fi>
2015-01-30 00:48:00 +0200

    some finetuning in login and layout

--- config/routes.rb
+++ config/routes.rb
@@ -65,7 +65,10 @@ Rails.application.routes.draw do
   #get 'ratings/new', to: 'ratings#new'
   #post 'ratings', to: 'ratings#create'
   get 'signup', to: 'users#new'
+  delete 'signout', to: 'sessions#destroy'
+  get 'signin', to: 'sessions#new'
 
   resources :ratings, only: [:index, :new, :create, :destroy]
   resource :session, only: [:new, :create, :delete]
+
 end
