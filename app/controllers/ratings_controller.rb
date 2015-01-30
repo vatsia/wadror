13b4003efac09ed3723aa494e9b74c8394d1d6d2
Olli Vatsia <olli.vatsia@helsinki.fi>
2015-01-30 02:29:38 +0200

    better error handling to new rating creation

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -9,10 +9,15 @@ class RatingsController < ApplicationController
   end
 
   def create
-    rating = Rating.create params.require(:rating).permit(:score, :beer_id)
-    session[:last_rating] = "#{rating.beer.name} #{rating.score} points"
-    current_user.ratings << rating
-    redirect_to ratings_path
+    @rating = Rating.create params.require(:rating).permit(:score, :beer_id)
+
+    if @rating.save
+      current_user.ratings << @rating
+      redirect_to user_path current_user
+    else
+      @beers = Beer.all
+      render :new
+    end
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   include AverageRating
 
+  validates :username, uniqueness: true
+  validates :username, length: {minimum: 3}
+
   has_many :ratings
 end
