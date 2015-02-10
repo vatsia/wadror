10b0c268d8af18362f91667b738ab4ee0b87e709
Olli Vatsia <olli.vatsia@cs.helsinki.fi>
2015-02-10 20:04:11 +0200

    tests for beermapping-api

--- spec/features/places_spec.rb
+++ spec/features/places_spec.rb
@@ -11,4 +11,31 @@ describe "Places" do
     click_button('Search')
     expect(page).to have_content("Oljenkorsi")
   end
+
+  it "if many are returned by API, are shown on the page" do
+    allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return(
+      [Place.new(name:"Oljenkorsi", id: 1),
+       Place.new(name:"Olotila", id: 2),
+       Place.new(name:"Pub Magneetti", id: 3)]
+    )
+
+
+    visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button('Search')
+
+    expect(page).to have_content("Oljenkorsi")
+    expect(page).to have_content("Olotila")
+    expect(page).to have_content("Pub Magneetti")
+  end
+
+  it "if many are returned by API, are shown on the page" do
+    allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return([])
+
+    visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button('Search')
+
+    expect(page).to have_content("No locations in kumpula")
+  end
 end
