cf72b1cc05efdde29c1c1a0baf2356048a57bd61
Olli Vatsia <olli.vatsia@cs.helsinki.fi>
2015-02-11 00:09:19 +0200

    more tests for beermapping-api

--- Gemfile
+++ Gemfile
@@ -61,6 +61,7 @@ group :test do
   gem 'capybara'
   gem 'launchy'
   gem 'simplecov', require: false
+  gem 'webmock'
 end
 
 gem 'httparty'
--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -4,7 +4,7 @@ class BeermappingApi
     response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
     places = response.parsed_response["bmp_locations"]["location"]
 
-    return [] if places_from_api.is_a? (Hash) and places['id'].nil?
+    return [] if places.is_a? (Hash) and places['id'].nil?
     places = [places] if places.is_a? (Hash)
     places.inject([]) do |set, place|
       set << Place.new(place)
